54f5738f95f135d4b4c74ea7f61911ff30e304df
Walk3r1 <105781742+Walk3r1@users.noreply.github.com>
2024-06-09 20:13:27 +0300

    columns and board

--- main.c
+++ main.c
@@ -24,3 +24,43 @@ void initialize_board(ConnectFour *game)
     }
     game->current_player = 'X';
 }
+
+
+void display_board(ConnectFour *game)
+{
+    for (int i = 0; i < game->height; ++i)
+    {
+        for (int j = 0; j < game->width; ++j)
+        {
+            printf("|%c", game->board[i][j]);
+        }
+        printf("|\n");
+    }
+    for (int j = 0; j < game->width; ++j)
+    {
+        printf("--");
+    }
+    printf("-\n");
+}
+
+int drop_token(ConnectFour *game, int column)
+{
+    if (column < 0 || column >= game->width)
+    {
+        return 0;
+    }
+    for (int i = game->height - 1; i >= 0; --i)
+    {
+        if (game->board[i][column] == ' ')
+        {
+            game->board[i][column] = game->current_player;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void switch_player(ConnectFour *game)
+{
+    game->current_player = (game->current_player == 'X') ? 'O' : 'X';
+}
